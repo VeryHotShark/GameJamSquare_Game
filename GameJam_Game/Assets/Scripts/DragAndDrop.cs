f9071c5eb878da419a53cf8fe0c55680cf6c5878
VeryHotShark <nguyen.tu.tony2000@gmail.com>
2019-04-26 22:29:34 +0200

    DragAndDropNew

--- GameJam_Game/Assets/Scripts/DragAndDrop.cs
+++ GameJam_Game/Assets/Scripts/DragAndDrop.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-
+    [SerializeField] float m_groundOffset = 0.2f;
+    [SerializeField] float m_yGrabOffset = 2;
+    [Space]
     [SerializeField] LayerMask draggableLayer;
+    [SerializeField] LayerMask workZoneLayer;
+    [Space]
     [SerializeField] Transform m_currentSelection;
+    [SerializeField] Transform m_landIndicator;
+
+    [SerializeField] Transform m_currentWorkZone;
+    Vector3 m_initWorkerPos;
 
     Camera m_cam;
 
-    Ray ray;
-    RaycastHit m_hitInfo;
+    Ray m_camRay;
+    RaycastHit m_camRayHitInfo;
+
+    Ray m_workerRay;
+    RaycastHit m_workerHitInfo;
 
     float m_selectionZValue;
 
@@ -26,11 +38,6 @@ public class DragAndDrop : MonoBehaviour
         m_cam = GetComponent<Camera>();
     }
 
-    void OnMouseRelease()
-    {
-        m_currentSelection = null;
-    }
-
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -41,12 +48,46 @@ public class DragAndDrop : MonoBehaviour
         if (m_currentSelection != null)
         {
             UpdateSelectionPosition();
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                OnMouseRelease();
+            }
         }
 
-         if (Input.GetMouseButtonUp(0))
+    }
+    
+    void OnMouseRelease()
+    {
+        bool hitWorkZone = CheckIfHitZone();
+
+        if(hitWorkZone)
         {
-            OnMouseRelease();
+            SnapToWorkZone();
         }
+        else
+        {
+            m_currentSelection.position = m_initWorkerPos;
+            m_landIndicator.position = new Vector3(m_initWorkerPos.x,m_landIndicator.position.y,m_initWorkerPos.z);
+        }
+        //m_currentSelection.position -= Vector3.up * m_yGrabOffset;
+        m_currentSelection = null;
+
+    }
+
+    bool CheckIfHitZone()
+    {
+        m_workerRay = new Ray(m_currentSelection.position,Vector3.down);
+
+        bool hitWorkZone = Physics.Raycast(m_workerRay, out m_workerHitInfo, 10f, workZoneLayer);
+
+        return hitWorkZone;
+    }
+
+    void SnapToWorkZone()
+    {
+        m_currentSelection.position = new Vector3(m_workerHitInfo.transform.position.x,m_initWorkerPos.y,m_workerHitInfo.transform.position.z);
+        m_landIndicator.position = new Vector3(m_workerHitInfo.transform.position.x,m_landIndicator.position.y,m_workerHitInfo.transform.position.z);
     }
 
     void OnMouseClick()
@@ -56,13 +97,16 @@ public class DragAndDrop : MonoBehaviour
 
     void CreateRay()
     {
-        ray = m_cam.ScreenPointToRay(Input.mousePosition);
+        m_camRay = m_cam.ScreenPointToRay(Input.mousePosition);
 
-        bool hitSomething = Physics.Raycast(ray, out m_hitInfo, 100f, draggableLayer);
+        bool hitSomething = Physics.Raycast(m_camRay, out m_camRayHitInfo, 100f, draggableLayer);
 
         if (hitSomething)
         {
-            m_currentSelection = m_hitInfo.transform;
+            m_currentSelection = m_camRayHitInfo.transform;
+            m_initWorkerPos = m_currentSelection.transform.position;
+            m_landIndicator.position = new Vector3(m_currentSelection.position.x,m_groundOffset,m_currentSelection.position.z);
+            m_currentSelection.position += Vector3.up * m_yGrabOffset;
         }
     }
 
@@ -75,5 +119,6 @@ public class DragAndDrop : MonoBehaviour
         mouseWorldPos = m_cam.ScreenToWorldPoint(mouseWorldPos);
 
         m_currentSelection.position = new Vector3(mouseWorldPos.x,m_currentSelection.position.y,mouseWorldPos.z);
+        m_landIndicator.position = new Vector3(mouseWorldPos.x,m_landIndicator.position.y,mouseWorldPos.z);
     }
 }
