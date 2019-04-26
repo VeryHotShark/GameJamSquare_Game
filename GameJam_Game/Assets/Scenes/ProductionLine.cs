14decc2865b0987845d9b1967265f544de7af13d
Belfaust <46440980+Belfaust@users.noreply.github.com>
2019-04-26 21:22:01 +0200

    SceneManagment

---
+++ GameJam_Game/Assets/Scenes/ProductionLine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductionLine : MonoBehaviour
+{
+    float ConveyerSpeed;
+    Vector3[] ProductionLines;
+    public GameObject Material;
+    void Update()
+    {
+        
+    }
+    public IEnumerator Production()
+    {
+        while(true)
+        {
+            Instantiate(Material, ProductionLines[0], Quaternion.identity,transform);
+        }
+
+    }
+}
