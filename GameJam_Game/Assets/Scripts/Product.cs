fee5455ed23526eb7fd11ddd82acaa34f2fbc367
Belfaust <46440980+Belfaust@users.noreply.github.com>
2019-04-26 23:47:14 +0200

    Linia Produkcji

---
+++ GameJam_Game/Assets/Scripts/Product.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Product : MonoBehaviour
+{
+    Vector3 currentTarget;
+    Vector3 direction;
+    bool Travel;
+    float dist;
+    public int i = 1;
+    void Start()
+    {
+        Debug.Log(transform.parent.GetComponent<ProductionLine>().ProductionLines.Length - 1);
+        
+    }
+    void Update()
+    {
+        currentTarget = transform.parent.GetComponent<ProductionLine>().ProductionLines[i].transform.position;
+        dist = Vector3.Distance(transform.position, currentTarget);
+        if (dist<.05f)
+        {
+           StartCoroutine( WaitForSomeTime(.5f));
+            i += 1;
+        }
+        if(i == transform.parent.GetComponent<ProductionLine>().ProductionLines.Length)
+        {
+            transform.position = transform.parent.GetComponent<ProductionLine>().ProductionLines[0].transform.position;
+        }
+        if (i > transform.parent.GetComponent<ProductionLine>().ProductionLines.Length - 1)
+            i = 0;
+
+        Vector3 dir = (currentTarget - transform.position).normalized;
+        if(Travel)
+        transform.Translate(dir * Time.deltaTime* transform.parent.GetComponent<ProductionLine>().ConveyerSpeed);
+    }
+
+    IEnumerator WaitForSomeTime(float amount)
+    {
+        Travel = false;
+        yield return new WaitForSeconds(amount);
+        Travel = true;
+    }
+}
---
+++ GameJam_Game/Assets/Scripts/ProductionLine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductionLine : MonoBehaviour
+{
+    public float ConveyerSpeed;
+    public GameObject[] ProductionLines;
+    public GameObject Material;
+    public int vectors;
+    private void Awake()
+    {
+        StartCoroutine(Production());
+    }
+    private void Update()
+    {
+        
+        if (Vector3.Distance(ProductionLines[ProductionLines.Length-1].transform.position,transform.GetChild(0).transform.position) < .25f)
+        {
+            StopAllCoroutines();
+        }
+    }
+    public IEnumerator Production()
+    {
+        while(true)
+        {
+            Instantiate(Material, ProductionLines[0].transform.position, Quaternion.identity,transform);
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(ProductionLines[vectors].transform.position, 0.3f);
+    }
+}
