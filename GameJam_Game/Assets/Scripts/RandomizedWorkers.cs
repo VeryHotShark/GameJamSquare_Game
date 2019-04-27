86adbedf05b76f564a16a3ef91702c9b90a8f37d
XardoX <jurek.gab.jg@gmail.com>
2019-04-27 17:01:07 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/VeryHotShark/GameJamSquare_Game""
    This reverts commit b3cb736c74bd7af2e720620e1df11419b4779af8.

---
+++ GameJam_Game/Assets/Scripts/RandomizedWorkers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizedWorkers : MonoBehaviour
+{   public int numberofworkers = 4;
+    public List<Worker> childsworkers = new List<Worker>();
+    public Worker worker;
+    void Start()
+    {
+        for(int i =0;i<numberofworkers;i++)
+        {
+            Worker child = Instantiate(worker, Vector3.zero, Quaternion.identity, transform) as Worker;
+            childsworkers.Add(child);
+            Randomizing_stats(i);
+        }
+    }
+    void Randomizing_stats(int j)
+    {
+        transform.GetChild(j).GetComponent<Worker>().dexterity = Random.Range(1, 4);
+        transform.GetChild(j).GetComponent<Worker>().intelligence = Random.Range(1, 4);
+        transform.GetChild(j).GetComponent<Worker>().strength = Random.Range(1, 4);
+    }
+}
