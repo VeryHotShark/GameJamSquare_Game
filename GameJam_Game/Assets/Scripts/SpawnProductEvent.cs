854c4a65cd03041b701b822446f18efd32dc6afe
Belfaust <46440980+Belfaust@users.noreply.github.com>
2019-04-27 21:10:52 +0200

    press

---
+++ GameJam_Game/Assets/Scripts/SpawnProductEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProductEvent : MonoBehaviour
+{
+    public void SpawnSand()
+    {
+        ProductionLine.Instance.SpawnProduct();
+
+    }
+}
--- GameJam_Game/Assets/Scripts/WorkPlace.cs
+++ GameJam_Game/Assets/Scripts/WorkPlace.cs
@@ -69,7 +69,7 @@ public class WorkPlace : MonoBehaviour
 
         m_currentWorker.SetWorkSpeed(finalWorkSpeed);
     }
-
+   
 
 
 }
