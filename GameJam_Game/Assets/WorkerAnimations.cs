5f0af09012174d9e96f223109e8499ae09e757d6
XardoX <jurek.gab.jg@gmail.com>
2019-04-27 23:38:26 +0200

    anim

---
+++ GameJam_Game/Assets/WorkerAnimations.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkerAnimations : MonoBehaviour
+{
+    public int index;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim.SetInteger("animIndex", index);
+    }
+}
