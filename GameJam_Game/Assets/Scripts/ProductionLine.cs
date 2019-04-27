9556e89acc0d80ee0b652440bae5f5600d65ceed
Belfaust <46440980+Belfaust@users.noreply.github.com>
2019-04-27 16:43:00 +0200

    Revert "Merge branch 'master' of https://github.com/VeryHotShark/GameJamSquare_Game"
    This reverts commit 1a5917b398bf446f56d38313e5ed846b07fb85cd, reversing
    changes made to f1636b95415f195f88f448cec145dc14e9506033.

--- GameJam_Game/Assets/Scripts/ProductionLine.cs
+++ GameJam_Game/Assets/Scripts/ProductionLine.cs
@@ -7,33 +7,30 @@ public class ProductionLine : MonoBehaviour
     public static ProductionLine Instance;
 
     public float spawnRate = 1f;
-    float flexiblespawn;
 
     public Transform[] ProductionLines ;
     public List<Product> childsproducts = new List<Product>();
     public Product product;
-    private Coroutine waypointHandle;
-
+    
     private void Awake()
     {
         Instance = this;
         StartCoroutine(Production());
     }
+  
     public IEnumerator Production()
     {
         while(true)
         {
-            yield return new WaitForSeconds(spawnRate);
             if (transform.childCount < 30)
             {
-                if (!Product.isWaiting&&!DragAndDrop.ActivePause)
+                if (!Product.isWaiting)
                 {
-                    
                     Product child = Instantiate(product, ProductionLines[0].transform.position, Quaternion.identity, transform) as Product;
                     childsproducts.Add(child);
                 }
             }
-            
+            yield return new WaitForSeconds(spawnRate);
         }
     }
 }
