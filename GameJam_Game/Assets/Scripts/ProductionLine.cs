2e217ee45f2442590d00259d716b0671601c13e0
VeryHotShark <nguyen.tu.tony2000@gmail.com>
2019-04-27 19:36:40 +0200

    Almost

--- GameJam_Game/Assets/Scripts/ProductionLine.cs
+++ GameJam_Game/Assets/Scripts/ProductionLine.cs
@@ -6,31 +6,37 @@ public class ProductionLine : MonoBehaviour
 {
     public static ProductionLine Instance;
 
+    public float yOffset;
     public float spawnRate = 1f;
 
-    public Transform[] ProductionLines ;
+    public Transform[] ProductionLines;
     public List<Product> childsproducts = new List<Product>();
     public Product product;
-    
+
     private void Awake()
     {
         Instance = this;
         StartCoroutine(Production());
     }
-  
+
     public IEnumerator Production()
     {
-        while(true)
+        while (true)
         {
-            if (transform.childCount < 30)
+            if (/*!Product.isWaiting && */ !ConveyerStartState.productInside)
             {
-                if (!Product.isWaiting)
-                {
-                    Product child = Instantiate(product, ProductionLines[0].transform.position, Quaternion.identity, transform) as Product;
-                    childsproducts.Add(child);
-                }
+                //Product child = Instantiate(product, ProductionLines[0].transform.position - Vector3.up * yOffset, Quaternion.identity, transform) as Product;
+                Product child = ProductPooler.Instance.GetPooledObject(ProductionLines[0].transform.position - Vector3.up * yOffset, Quaternion.identity);
+                childsproducts.Add(child);
             }
+
             yield return new WaitForSeconds(spawnRate);
         }
+    }
+
+    public void SpawnProduct()
+    {
+        Product child = ProductPooler.Instance.GetPooledObject(ProductionLines[0].transform.position - Vector3.up * yOffset, Quaternion.identity);
+        childsproducts.Add(child);
     }
 }
